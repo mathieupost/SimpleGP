93357382774bb1909cdddf438d63db7d06df3e05
Kasper Wendel <kasperwendel@gmail.com>
2019-06-19 15:15:15 +0200

    population size test

--- test_gp_pop_size.py
+++ test_gp_pop_size.py
@@ -1,23 +1,26 @@
 # Libraries
 
 import random
+import sys
 
 from cross_validation import CrossValidation
 from simplegp.Evolution.Evolution import SimpleGP
-# Internal imports
 from simplegp.Nodes.SymbolicRegressionNodes import *
 from simplegp.Weights.Tuner import Tuner
 
 np.random.seed(42)
 random.seed(42)
 
-# Set functions and terminals
-functions = [AddNode(), SubNode(), MulNode(), AnalyticQuotientNode()]  # chosen function nodes
-terminals = [EphemeralRandomConstantNode()]  # use one ephemeral random constant node
+settings = [2, 10, 100, 500, 1000, 2000]
 
-# Run GP
+for pop in settings:
+    sys.stdout = open(f"log/log_pop_size_{pop}.txt", "w+")
+    # Set functions and terminals
+    functions = [AddNode(), SubNode(), MulNode(), AnalyticQuotientNode()]  # chosen function nodes
+    terminals = [EphemeralRandomConstantNode()]  # use one ephemeral random constant node
 
-tuner = Tuner()
-sgp = SimpleGP(tuner=tuner, functions=functions, pop_size=100, max_generations=100)  # other parameters are optional
+    # Run GP
+    tuner = Tuner()
+    sgp = SimpleGP(tuner=tuner, functions=functions, pop_size=pop, max_generations=100)  # other parameters are optional
 
-CrossValidation(sgp, terminals).validate()
+    CrossValidation(sgp, terminals).validate()
