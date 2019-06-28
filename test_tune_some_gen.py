a30e6dff88ab8ca8d49b781247ce5f7edd9ca9c2
Kasper Wendel <kasperwendel@gmail.com>
2019-06-28 19:44:43 +0200

    test for running in some generations

---
+++ test_tune_some_gen.py
@@ -0,0 +1,63 @@
+# Libraries
+
+import random
+import sys
+
+from tqdm import tqdm
+
+from cross_validation import CrossValidation
+from logger.multi_logger import MultiLogger
+from simplegp.Evolution.Evolution import SimpleGP
+from simplegp.Nodes.SymbolicRegressionNodes import *
+from simplegp.Weights.Tuner import Tuner
+
+np.random.seed(42)
+random.seed(42)
+
+settings = [
+    # Thieu
+    # Run every 5 generations (gen starts with 0, so gen 4 is actually the 5th generation)
+    (False, range(4, 100, 5), "tune_5_gen"),
+
+    # Kasper
+    # Run every 5 generations with ls
+    (True, range(4, 100, 5), "tune_5_gen_ls"),
+
+    # Timo
+    # Run every 20 generations
+    (False, range(19, 100, 20), "tune_20_gen"),
+
+    # Sven
+    # Run every 20 generations with ls
+    (False, range(19, 100, 20), "tune_20_gen_ls")
+]
+
+
+def run_in_gen(ls, run_gen):
+    # Set functions and terminals
+    functions = [AddNode(), SubNode(), MulNode(), AnalyticQuotientNode()]  # chosen function nodes
+    terminals = [EphemeralRandomConstantNode()]  # use one ephemeral random constant node
+
+    # Run GP
+    tuner = Tuner(
+        scale_range=(-5, 5),
+        translation_range=(-5, 5),
+        run_generations=(run_gen)
+    )
+    sgp = SimpleGP(
+        linear_scale=ls,
+        tuner=tuner,
+        functions=functions,
+        pop_size=100,
+        max_generations=20
+    )
+
+    CrossValidation(sgp, terminals).validate()
+
+
+if __name__ == '__main__':
+    for setting in tqdm(settings, desc="Test Weights"):
+        ls, run_gen, name = setting
+        with open(f"log/log_{name}.txt", "w+") as logfile:
+            mc = MultiLogger([sys.stdout, logfile])
+            mc.capture(run_in_gen, ls, run_gen)
