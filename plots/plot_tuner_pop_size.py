0df5885a7e9faa4841dbbb33cdf7a9100c217ee9
Kasper Wendel <kasperwendel@gmail.com>
2019-06-29 16:55:25 +0200

    tuner pop size plots

--- plots/plot_tuner_pop_size.py
+++ plots/plot_tuner_pop_size.py
@@ -1,47 +1,107 @@
-# sphinx_gallery_thumbnail_number = 3
-import matplotlib.pyplot as plt
+# %%
 import numpy as np
+
+from reader import avg_over_runs
+
+runs = 10
+pop_size = 100
 
 
 def extract_numbers(size):
     with open(f"../log/log_tuner_pop_size_{size}.txt", "r") as file_handle:
         line_list = file_handle.readlines()
 
-    stopped_at = 0
-    mse_sum = 0
-    mse_items = 0
+    mse = np.zeros((runs, pop_size))
+    iterations = np.zeros((runs, pop_size))
+    # TODO: analyse the time it took
+    evaluations = np.zeros(runs)
+
+    run = None
+    pop = None
+
     for line in line_list:
+        line = line.rstrip("\n")
+
+        # Parse the cross validation number
+        if "Run" in line:
+            run = int(line[-1])
+            pop = 0
+            stopped_early = False
+
+        # Parse tuner logs
         if "Tuner" in line:
             if "converged" in line:
-                stopped_at += int(line.split(' ')[-1])
+                split = line.split(' ')
+                iterations[run, pop] = split[-1]
+                stopped_early = True
             else:
-                mse_sum += (float(line.split(' ')[-2]) - float(line.split(' ')[-1]))
-                mse_items += 1
-                stopped_at += 100
+                split = line.split(' ')
 
-    print(stopped_at / mse_items)
-    print(mse_sum / mse_items)
-    print()
+                if not stopped_early:
+                    # Max iteration was reached thus 100
+                    iterations[run, pop] = 100
+
+                mse[run, pop] = float(split[-1])
+                stopped_early = False
+                pop += 1
+
+        if "evaluations" in line:
+            split = line.split(' ')
+            evaluations[run] = split[0]
+
+    # Average over the population
+    mse = np.mean(mse, axis=1)
+    iterations = np.mean(iterations, axis=1)
+
+    return mse, iterations, evaluations
 
 
+data = {}
 sizes = [10, 20, 50, 100]
+# sizes = [50, 100]
 
 for size in sizes:
-    extract_numbers(size)
+    data[size] = extract_numbers(size)
 
-# plt.figure()
-#
-# plt.plot(np.log10(sizes), y, color='blue', label='MSE training')
-# plt.fill_between(np.log10(sizes), y_lower, y_upper, color='blue', alpha='0.1')
-#
-# plt.xticks(np.log10(sizes), sizes)
-#
-# plt.plot(np.log10(sizes), z, color='red', label='MSE test')
-# plt.fill_between(np.log10(sizes), z_lower, z_upper, color='red', alpha='0.1')
-#
-# plt.xlabel('Population size')
-# plt.ylabel('MSE')
-#
-# plt.title("Simple GP: MSE vs. population size")
-# plt.savefig("../images/plot_gp_pop_size.png")
-# plt.show()
+# %%
+
+import matplotlib.pyplot as plt
+
+# Set theme
+import matplotlib
+
+matplotlib.style.use('seaborn-darkgrid')
+matplotlib.rcParams['font.family'] = "serif"
+
+# Create y values
+ys_mse = np.zeros_like(sizes)
+ys_evals = np.zeros_like(sizes)
+for idx, s in enumerate(sizes):
+    mse, _, evals = data[s]
+    m_mse, _ = avg_over_runs(mse)
+    m_eval, _ = avg_over_runs(evals)
+
+    ys_mse[idx] = m_mse
+    ys_evals[idx] = m_eval
+
+plt.figure()
+plt.plot(sizes, ys_mse, color='blue', label="MSE after tuning")
+
+plt.xlabel("Tuner population size")
+plt.ylabel("MSE")
+plt.title("Tuner: MSE vs population size")
+plt.legend()
+
+plt.savefig("../images/tuner_pop_size_mse")
+plt.show()
+
+plt.figure()
+plt.plot(sizes, ys_evals, color='blue', label="Total evaluations")
+
+plt.xlabel("Tuner population size")
+plt.ylabel("Evaluations")
+plt.title("Tuner: evaluations vs population size")
+plt.legend()
+
+plt.savefig("../images/tuner_pop_size_evals")
+plt.show()
