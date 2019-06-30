ccfc3d0a07cd911dc73de1e0ab57c75dd7975329
Kasper Wendel <kasperwendel@gmail.com>
2019-06-30 13:47:19 +0200

    finished plots for some gens runs

--- plots/plot_some_gen.py
+++ plots/plot_some_gen.py
@@ -11,28 +11,27 @@ matplotlib.rcParams['font.family'] = "serif"
 
 # Global settings of the experiments
 runs = 10
-gens = 20
+gens = 100
 pop_size = 100
 logs = [
-    # TODO: add data from log below when finished
-    # ("../log/log_tune_5_gen.txt", "5 gen"),
-    ("../log/log_tune_5_gen_ls.txt", "5 gen - LS"),
-    ("../log/log_tune_20_gen.txt", "20 gen"),
-    ("../log/log_tune_20_gen_ls.txt", "20 gen - LS"),
+    ("../log/log_tune_5_gen_max_gen_100.txt", "5 gen"),
+    ("../log/log_tune_5_gen_ls_max_gen_100.txt", "5 gen - LS"),
+    ("../log/log_tune_20_gen_max_gen_100.txt", "20 gen"),
+    ("../log/log_tune_20_gen_ls_max_gen_100.txt", "20 gen - LS"),
 ]
 
 data = extract_all_data(logs, runs=runs, gens=gens, pop_size=pop_size)
 x = np.arange(1, gens + 1, 1)
 
-#%%
+# %%
 # Plot best fitness per generation
 palette = plt.get_cmap('tab10')
 
 plt.figure()
 plt.xlabel("Generation")
 plt.ylabel("Best Training MSE")
-plt.xticks(x)
 plt.title("Tune after x generations: best fitness per generation")
+plt.yscale("log")
 
 color = 0
 for w in data.keys():
@@ -47,7 +46,7 @@ plt.legend()
 plt.savefig("../images/tune_gen_fitness")
 plt.show()
 
-#%%
+# %%
 
 palette = plt.get_cmap('tab10')
 
@@ -76,50 +75,102 @@ plt.show()
 
 # Plots for 5gens tuning and 20gens tuning can better be seperated...
 
-gens5 = ['5 gen - LS']
-x_gens5 = [4, 9, 14, 19]
-gens20 = ['20 gen', '20 gen - LS']
-x_gens20 = [19]
+x_gens5 = list(range(4, 100, 5))
+x_gens20 = list(range(19, 100, 20))
 
 palette = plt.get_cmap('tab20')
 color = 0
 
 
-def plot_gen_diff(x, keys, name, color):
-    # Plot the average improvement with weight tuning
+def get_before(key):
+    _, _, before, _ = data[key]
+    m_before, std_before = avg_over_runs(before)
+
+    y = m_before[m_before > 0]
+    std = std_before[std_before > 0]
+
+    return y, std
+
+
+def get_after(key):
+    _, _, _, after = data[key]
+    m_after, std_after = avg_over_runs(after)
+
+    y = m_after[m_after > 0]
+    std = std_after[std_after > 0]
+
+    return y, std
+
+
+# %% Plot average fitness before
+
+def plot_before(keys, tune_gens, name):
     plt.figure()
     plt.xlabel("Generation")
     plt.ylabel("Training MSE")
-    plt.xticks(x)
-    plt.title("Weight Tuning: fitness before and after tuning")
+    plt.title("Tune after x generations: average fitness before")
+    plt.yscale("log")
 
-    # plt.xlim([1, 20])
-    # plt.ylim([0, 1500])
 
-    for w in keys:
-        _, _, before, after = data[w]
+    palette = plt.get_cmap('tab10')
 
-        # Mask zeros values
-        before = before[before > 0]
-        after = after[after > 0]
-        print(before.shape)
+    color = 0
 
-        m_before, std_before = avg_over_runs(before)
+    for k, gens in zip(keys, tune_gens):
+        y_before, std_before = get_before(k)
 
-        m_after, std_after = avg_over_runs(after)
+        x = np.arange(len(gens))
 
-        plt.plot(x, m_before, color=palette(color), label=f"Before -{w}")
-        # plt.fill_between(x, m_before + std_before, m_before - std_before, color=palette(color), alpha=0.1)
+        plt.plot(x, y_before, color=palette(color), label=f"{k}")
+        plt.fill_between(x, y_before + std_before, y_before - std_before, color=palette(color), alpha=0.1)
 
-        plt.plot(x, m_after, color=palette(color + 1), label=f"After -{w}")
-        # plt.fill_between(x, m_after + std_after, m_after - std_after, color=palette(color + 1), alpha=0.1)
+        plt.xticks(x, gens)
 
-        color += 2
+        color += 1
 
+    # Log scale so log(1) = 0
+    plt.ylim([1, plt.ylim()[1]])
     plt.legend()
-    plt.savefig(f"../images/tune_gen_{name}")
+    plt.savefig(f"../images/{name}")
     plt.show()
 
-plot_gen_diff(x_gens5, gens5, "gens5", 0)
-plot_gen_diff(x_gens20, gens20, "gens20", 2)
 
+## Seperate plots of 20 gens and 5 gens
+plot_before(['5 gen', '5 gen - LS'], list([x_gens5, x_gens5]), "tune_5_gen_before")
+plot_before(['20 gen', '20 gen - LS'], list([x_gens20, x_gens20]), "tune_20_gen_before")
+
+
+# %% Plot after fitness after tuning
+
+
+def plot_after(keys, tune_gens, name):
+    plt.figure()
+    plt.xlabel("Generation")
+    plt.ylabel("Training MSE")
+    plt.title("Tune after x generations: average fitness after")
+
+    palette = plt.get_cmap('tab10')
+
+    color = 0
+
+    for k, gens in zip(keys, tune_gens):
+        y_after, std_after = get_after(k)
+
+        x = np.arange(len(gens))
+
+        plt.plot(x, y_after, color=palette(color), label=f"{k}")
+        plt.fill_between(x, y_after + std_after, y_after - std_after, color=palette(color), alpha=0.1)
+
+        plt.xticks(x, gens)
+
+        color += 1
+
+    plt.ylim([0, plt.ylim()[1]])
+    plt.legend()
+    plt.savefig(f"../images/{name}")
+    plt.show()
+
+
+## Seperate plots of 20 gens and 5 gens
+plot_after(['5 gen', '5 gen - LS'], list([x_gens5, x_gens5]), "tune_5_gen_after")
+plot_after(['20 gen', '20 gen - LS'], list([x_gens20, x_gens20]), "tune_20_gen_after")
